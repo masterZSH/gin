afcd41ffb40737f87c562d627d017729146bf115
masterZSH <104438932@qq.com>
2020-09-07 22:00:53 +0800

    test:添加query绑定部分测试用例

---
+++ binding/query_test.go
@@ -0,0 +1,33 @@
+// Copyright 2017 Manu Martinez-Almeida.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package binding
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestName(t *testing.T) {
+	var qb queryBinding
+	assert.Equal(t, qb.Name(), "query")
+}
+
+type TestQuery struct {
+	Test string `form:"test"`
+}
+
+func TestBind(t *testing.T) {
+	var qb queryBinding
+	var obj TestQuery
+	r, _ := http.NewRequest("get", "/", nil)
+	q := r.URL.Query()
+	q.Set("test", "foo")
+	r.URL.RawQuery = q.Encode()
+	err = qb.Bind(r, &obj)
+	assert.Nil(t, err)
+	assert.Equal(t, "foo", obj.Test)
+}
